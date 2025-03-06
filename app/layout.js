bcea53a6223f42f10f22898d34dd0bbf6ddb445a
Ana Julia <anajuliademattei26@gmail.com>
2025-03-06 11:07:18 -0300

    ajustes

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
 import "../styles/global.css";
+
 			export const metadata = {
 				title: "My First NextJS App",
 			};
+            
 			export default function RootLayout({ children }) {
 				return (
 					<html>
