234f0d7b9d0f49649f40e45e56c03180001bb680
Ana Julia <anajuliademattei26@gmailcom>
2025-03-20 09:56:42 -0300

    Finish

--- app/cadastro/page.jsx
+++ app/cadastro/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import "./cadastro.css";
+import { useState } from "react";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { useRouter } from "next/navigation";
@@ -7,10 +8,15 @@ import { useRouter } from "next/navigation";
 export default function Cadastro() {
     const router = useRouter();
     
+    const [input1, setInput1] = useState("");
+    const [input2, setInput2] = useState("");
+    const [match, setMatch] = useState(null);
+    const [error, setError] = useState(null);
+
     const handleCheck = () => {
         if (input1 !== input2) {
             setMatch(false);
-            setError("As senhas não conferem");
+            setError("As senhas não conferem!");
         } else if (input1.length < 6) {
             setMatch(false);
             setError("A senha deve ter no mínimo 6 caracteres");
@@ -22,7 +28,7 @@ export default function Cadastro() {
             setError("Preencha o input 2");
         } else {
             setMatch(true);
-            setError("Sucesso");
+            setError("Sucesso!");
         }
     };
 
@@ -31,12 +37,13 @@ export default function Cadastro() {
             <div className="cadastro">
                 <h1>CADASTRO</h1>
                 <h3>Crie sua conta</h3>
-                <Input placeholder="Nome" />
-                <Input placeholder="Email" type="email" />
+                <input type="email" placeholder="E-mail"  />
+                <input type="text" placeholder="Nome de Usuário"/>
                 <Input placeholder="Data de Nascimento" type="date" />
-                <Input placeholder="Senha" type="password" />
-                <Input placeholder="Confirme sua senha" type="password" />
-                <Button text="Cadastrar" onClick={() => alert('Cadastro realizado!')} />
+                <input type="password" placeholder="Digite a senha" value={input1} onChange={(e) => setInput1(e.target.value)} />
+                <input type="password" placeholder="Repita a senha" value={input2} onChange={(e) => setInput2(e.target.value)} />
+                <button className="cadastrar-button" onClick={handleCheck}>Cadastrar</button>{" "}
+                {match !== null && <p>{error}</p>}{" "}
                 <Button text="Voltar" onClick={() => router.push('/login')} />
             </div>
         </div>
